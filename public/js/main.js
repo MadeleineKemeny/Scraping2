b45b484a05ab4cecf89ce4c4ef787d94df6ab5c7
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-08-28 20:04:34 -0500

    public folder beginning pieces

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+function scrape() {
+    $.ajax({
+        url: "/api/scrape",
+        method: "POST"
+    }).then(function (response) {
+        alert("Get ready... " + response.count + " news items are saved to your database");
+        location.reload();
+    });
+}
+
+
+function deleteComment(newsId, commentId) {
+    $.ajax({
+        url: '/news/' + newsId + '/comments/' + commentId,
+        method: 'DELETE'
+    }).then(function (response) {
+        location.reload();
+    });
+}
